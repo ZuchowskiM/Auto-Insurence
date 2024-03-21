9f97a9833a6abf0e8208386b03745c58e070466e
ZuchowskiM <m.zuchowski24@gmail.com>
2024-03-21 20:18:06 +0100

    frontend navbar setup

---
+++ auto-insurence-frontend/src/components/navbar.tsx
@@ -0,0 +1,52 @@
+import "./navbar.scss"
+import { Link } from "react-router-dom"
+import { DarkMode, LightMode, Menu } from "@mui/icons-material"
+import { ToggleButton } from "@mui/material"
+import { ThemeContext } from "../context/theme.context"
+import { useContext, useState } from "react"
+
+const links = [
+    { href: "/", label: "Home", },
+    { href: "/cars", label: "Cars", },
+    { href: "/carowners", label: "Car Owners", },
+]
+
+const Navbar = () => {
+    const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+    const [open, setOpen] = useState<boolean>(false);
+
+    const ToggleOpenMenu = () => {
+        setOpen((prevState) => !prevState);
+    }
+
+    const menuStyles = open ? "menu open" : "menu";
+
+    return (
+        <div className="navbar">
+            <div className="brand">
+                <span>Car insurence</span>
+            </div>
+            <div className={menuStyles}>
+                <ul>
+                    {
+                        links.map(item => (
+                            <li key={item.href} onClick={ToggleOpenMenu}>
+                                <Link to={item.href}>{item.label}</Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+            <div className="hamburger">
+                <Menu onClick={ToggleOpenMenu}/>
+            </div>
+            <div className="toggle">
+                <ToggleButton value={"check"} selected={darkMode} onChange={toggleDarkMode}>
+                    {{ darkMode } ? <DarkMode /> : <LightMode />}
+                </ToggleButton>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ auto-insurence-frontend/src/context/theme.context.tsx
@@ -0,0 +1,29 @@
+import { createContext, useState } from "react"
+
+interface IThemeContext {
+    darkMode: boolean;
+    toggleDarkMode: () => void;
+}
+
+interface IThemeContextProviderProps {
+    children: React.ReactNode,
+}
+
+export const ThemeContext = createContext<IThemeContext>({
+    darkMode: false,
+    toggleDarkMode: () => { },
+});
+
+const ThemeContextProvider = ({ children }: IThemeContextProviderProps) => {
+    const [darkMode, setDarkMode] = useState<boolean>(false);
+
+    const toggleDarkMode: () => void = () => {
+        setDarkMode(prevState => !prevState);
+    }
+
+    return <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+        {children}
+    </ThemeContext.Provider>
+};
+
+export default ThemeContextProvider;
